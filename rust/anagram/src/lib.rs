6bb738a923263eceada75aeda8b464d33df77136
Colton Crowe <coltontcrowe@gmail.com>
2019-10-04 04:05:50 -0700

    currently working on anagram.

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -3,12 +3,13 @@ use std::collections::{HashMap, HashSet};
 /// Returns a HashSet of the words in possible that are anagrams of
 /// word (ignores case)
 pub fn anagrams_for<'a>(word: &str, possible: &[&'a str]) -> HashSet<&'a str> {
-    let freq = occurances(&word);
+    let freq = occurances(word);
 
     possible
         .iter()
+        .map(|other| &'a other.to_lowercase().as_str())
         .filter(|&other| occurances(other) == freq)
-        .filter(|&other| other.to_lowercase() != word.to_lowercase())
+        .filter(|&other| other != &word.to_lowercase())
         .copied()
         .collect()
 }
@@ -16,10 +17,9 @@ pub fn anagrams_for<'a>(word: &str, possible: &[&'a str]) -> HashSet<&'a str> {
 /// Given a word, returns a HashMap recording how frequently each
 /// char occurs ignoring case
 fn occurances(word: &str) -> HashMap<char, u32> {
-    let lower = word.to_lowercase();
     let mut frequency: HashMap<char, u32> = HashMap::new();
 
-    for c in lower.chars() {
+    for c in word.chars() {
         let count = frequency.entry(c).or_insert(0);
         *count += 1;
     }
@@ -33,8 +33,8 @@ mod occurances_tests {
     use std::collections::HashMap;
 
     #[test]
-    fn basic_test() {
-        // test a regular word
+    fn test_word_foo() {
+        // test a regular lowercase word
         let word = "foo";
         let freq: HashMap<char, u32> = [('f', 1), ('o', 2)].iter().cloned().collect();
 
@@ -42,7 +42,7 @@ mod occurances_tests {
     }
 
     #[test]
-    fn uses_lower() {
+    fn test_word_OoPs() {
         // test a word with mixed capital and lowercase letters
         let word = "OoPs";
         let freq: HashMap<char, u32> = [('o', 2), ('p', 1), ('s', 1)].iter().cloned().collect();
@@ -51,7 +51,7 @@ mod occurances_tests {
     }
 
     #[test]
-    fn repeated() {
+    fn test_a_1000_times() {
         // test a very long string
         let word = &['a'; 1000].iter().collect::<String>();
         let freq: HashMap<char, u32> = [('a', 1000)].iter().cloned().collect();
