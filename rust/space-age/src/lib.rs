64f822ccd85cfaf5dcacae3677eb537750f3b537
Colton <ctc87@cornell.edu>
2019-11-08 23:16:36 -0800

    Implemented mentor's suggestions for space age.  It uses macros now which cuts down on a lot of the repeated lines of code.

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -71,7 +71,7 @@ pub trait Planet {
     /// extern crate assert_float_eq;
     /// use assert_float_eq::*;
     /// use space_age::*;
-    //
+    ///
     /// let earth_year = Duration::from(31_557_600);
     /// assert_float_absolute_eq!(Earth::years_during(&earth_year), 1.0);
     ///
@@ -79,47 +79,33 @@ pub trait Planet {
     /// assert_float_absolute_eq!(Mercury::years_during(&mercury_year), 1.0);
     /// ```
     fn years_during(d: &Duration) -> f64 {
-        d.sec / Self::SECONDS_PER_EARTH_YR / Self::EARTH_YRS_PER_PLANET_YR
+        let earth_years = d.sec / Self::SECONDS_PER_EARTH_YR;
+        let planet_years =  earth_years / Self::EARTH_YRS_PER_PLANET_YR;
+        planet_years
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 0.240_846_7;
+/// Creates a struct that implements the Planet trait and assigns
+/// an associated constant named EARTH_YRS_PER_PLANET_YR
+///
+/// # Arguments
+///
+/// * `name` - The name of the struct
+/// * `earth_yrs` - The number of earth years in a planet year
+macro_rules! new_planet {
+    ($name:ident, $earth_yrs:expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            const EARTH_YRS_PER_PLANET_YR: f64 = $earth_yrs;
+        }
+    }
 }
 
-impl Planet for Venus {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 0.615_197_26;
-}
-
-impl Planet for Earth {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 1.0;
-}
-
-impl Planet for Mars {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 1.880_815_8;
-}
-
-impl Planet for Jupiter {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 11.862_615;
-}
-
-impl Planet for Saturn {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 29.447_498;
-}
-
-impl Planet for Uranus {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 84.016_846;
-}
-
-impl Planet for Neptune {
-    const EARTH_YRS_PER_PLANET_YR: f64 = 164.79132;
-}
+new_planet!(Mercury, 0.240_846_700);
+new_planet!(Venus, 0.615_197_260);
+new_planet!(Earth, 1.0);
+new_planet!(Mars, 1.880_815_800);
+new_planet!(Jupiter, 11.862_615);
+new_planet!(Saturn, 29.447_498);
+new_planet!(Uranus, 84.016_846);
+new_planet!(Neptune, 164.791_320);
