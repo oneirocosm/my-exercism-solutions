b8ac2cf783c1110d79262708c2fb6f5efae6ca22
Colton Crowe <coltontcrowe@gmail.com>
2020-04-18 06:04:30 -0700

    Completed proverb exercise on the rust track

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,18 @@
+use itertools::Itertools;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.len() == 0 {
+        return String::new();
+    }
+
+    let mut proverb = String::new();
+    for (the_want, the_lost) in list.iter().tuple_windows() {
+        proverb.push_str(&format!(
+            "For want of a {} the {} was lost.\n",
+            the_want, the_lost,
+        ));
+    }
+    proverb.push_str(&format!("And all for the want of a {}.", list[0]));
+
+    proverb
 }
