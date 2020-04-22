e0358e8954ac43a6f9ce4e2b0d892a6ea4bb46e7
Colton Crowe <coltontcrowe@gmail.com>
2020-04-22 03:30:28 -0700

    Completed perfect-numbers exercise on the rust track.  Note that this solution is a bit needlessly complex.

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -6,5 +6,45 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 {
+        return None;
+    }
+
+    match Factors::from(num).into_iter().sum::<u64>() {
+        x if x < num => Some(Classification::Deficient),
+        x if x > num => Some(Classification::Abundant),
+        _ => Some(Classification::Perfect),
+    }
+}
+
+struct Factors {
+    value: u64,
+    current: Option<u64>,
+}
+
+impl Factors {
+    fn from(value: u64) -> Self {
+        Factors {
+            value,
+            current: None,
+        }
+    }
+}
+
+impl Iterator for Factors {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let new = match (self.value, self.current) {
+            (0, _) => None,
+            (1, _) => None,
+            (_, Some(old)) => ((old + 1)..self.value)
+                .filter(|x| self.value % x == 0)
+                .take(1)
+                .last(),
+            (_, None) => Some(1),
+        };
+        self.current = new;
+        new
+    }
 }
