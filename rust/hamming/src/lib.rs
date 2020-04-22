81403daa34ebc7cea48cafc326a19992e6d15e19
Colton Crowe <coltontcrowe@gmail.com>
2020-04-22 03:48:57 -0700

    Completed hamming exercise on rust track

---
+++ rust/hamming/src/lib.rs
@@ -0,0 +1,12 @@
+/// Return the Hamming distance between the strings,
+/// or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    if s1.len() != s2.len() {
+        return None;
+    }
+    Some(
+        s1.chars()
+            .zip(s2.chars())
+            .fold(0, |acc, (c1, c2)| if c1 != c2 { acc + 1 } else { acc }),
+    )
+}
