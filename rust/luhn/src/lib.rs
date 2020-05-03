6d158aaf23118d3b1ca34501c49067aabd671cd7
Colton Crowe <coltontcrowe@gmail.com>
2020-05-03 16:30:07 -0700

    First attempt at luhn  exercise on the rust track

---
+++ rust/luhn/src/lib.rs
@@ -0,0 +1,42 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    match calc_luhn_sum(code) {
+        None => false,
+        Some(sum) => sum % 10 == 0,
+    }
+}
+
+/// Calculates the sum of all digits from a luhn
+/// number as Option<32>.  Returns None if the
+/// given code is invalid
+fn calc_luhn_sum(code: &str) -> Option<u32> {
+    let valid_digits = code
+        .chars()
+        .filter_map(|digit| match digit {
+            ' ' => None,
+            d => Some(d.to_digit(10)),
+        })
+        .collect::<Option<Vec<_>>>()?;
+
+    if valid_digits.len() < 2 {
+        return None;
+    }
+
+    let luhn_sum = valid_digits
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(i, x)| parse_digit(i, x))
+        .sum::<u32>();
+    Some(luhn_sum)
+}
+
+/// Convert each digit to the proper value required
+/// to compute the luhn sum.
+fn parse_digit(index: usize, digit: u32) -> u32 {
+    match (index % 2 == 1, digit) {
+        (true, x) if x < 5 => x * 2,
+        (true, x) => x * 2 - 9,
+        (false, x) => x,
+    }
+}
