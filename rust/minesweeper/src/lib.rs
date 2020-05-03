976afa692d61035f2f74a44e88fd4df120edc6e5
Colton Crowe <coltontcrowe@gmail.com>
2020-05-03 16:30:58 -0700

    Made significant simplification in the implementation of the minesweeper exercise on the rust track.  Accepted by mentor.

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,78 +1,40 @@
 struct BoardState {
-    n_cols: usize,
-    n_mines: Vec<Option<u8>>,
+    n_mines: Vec<Vec<Option<u8>>>,
 }
 
 impl BoardState {
     fn new(n_rows: usize, n_cols: usize) -> Self {
-        let size: usize = n_rows * n_cols;
         BoardState {
-            n_cols,
-            n_mines: vec![Some(0); size],
+            n_mines: vec![vec![Some(0); n_cols]; n_rows],
         }
     }
 
-    fn update_surround(&mut self, index: usize) {
+    fn update_surround(&mut self, index: (usize, usize)) {
+        let (mid_row, mid_col) = index;
+        let top = mid_row.wrapping_sub(1);
+        let bottom = mid_row.wrapping_add(1);
+        let left = mid_col.wrapping_sub(1);
+        let right = mid_col.wrapping_add(1);
 
-        let to_update = self.find_squares_to_update(index);
-
-        self.n_mines[index] = None;
-        for surround in to_update {
-            self.n_mines[surround] = self.n_mines[surround].map(|count| count + 1);
-        }
+        self.increment(top, left);
+        self.increment(top, mid_col);
+        self.increment(top, right);
+        self.increment(mid_row, left);
+        self.n_mines[mid_row][mid_col] = None;
+        self.increment(mid_row, right);
+        self.increment(bottom, left);
+        self.increment(bottom, mid_col);
+        self.increment(bottom, right);
     }
 
-    fn find_squares_to_update(&self, index: usize) -> Vec<usize> {
-        let has_left = self.has_left(index);
-        let has_right = self.has_right(index);
-        let has_top = self.has_top(index);
-        let has_bottom = self.has_bottom(index);
-        
-        let mut to_update: Vec<usize> = Vec::new();
-        if has_left && has_top {
-            to_update.push(index - self.n_cols - 1);
+    fn increment(&mut self, row_index: usize, col_index: usize) -> Option<()> {
+        match self.n_mines.get_mut(row_index)?.get_mut(col_index)? {
+            None => {}
+            Some(mine) => *mine += 1,
         }
-        if has_top {
-            to_update.push(index - self.n_cols);
-        }
-        if has_right && has_top {
-            to_update.push(index - self.n_cols + 1);
-        }
-        if has_left {
-            to_update.push(index - 1);
-        }
-        if has_right {
-            to_update.push(index + 1);
-        }
-        if has_left && has_bottom {
-            to_update.push(index + self.n_cols - 1);
-        }
-        if has_bottom {
-            to_update.push(index + self.n_cols);
-        }
-        if has_right && has_bottom {
-            to_update.push(index + self.n_cols + 1);
-        }
-
-        to_update
+        Some(())
     }
 
-    fn has_left(&self, index: usize) -> bool {
-        index % self.n_cols != 0
-    }
-
-    fn has_right(&self, index: usize) -> bool {
-        (index + 1) % self.n_cols != 0
-    }
-
-    fn has_top(&self, index: usize) -> bool {
-        index >= self.n_cols
-    }
-
-    fn has_bottom(&self, index: usize) -> bool {
-        let total = self.n_mines.len();
-        index < total - self.n_cols
-    }
     fn parse_square(square: Option<u8>) -> char {
         match square {
             None => '*',
@@ -84,14 +46,13 @@ impl BoardState {
 
 impl Into<Vec<String>> for BoardState {
     fn into(self) -> Vec<String> {
-        let n_mines: Vec<char> = self
-            .n_mines
+        self.n_mines
             .into_iter()
-            .map(Self::parse_square)
-            .collect();
-        n_mines
-            .chunks(self.n_cols)
-            .map(|row| row.iter().collect())
+            .map(|row| {
+                row.iter()
+                    .map(|x| Self::parse_square(*x))
+                    .collect::<String>()
+            })
             .collect()
     }
 }
@@ -102,18 +63,25 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     if n_rows == 0 {
         return Vec::new();
     }
-    let n_cols = if n_rows > 0 { minefield[0].len() } else { 0 };
+    let n_cols = minefield[0].len();
     if n_cols == 0 {
         return vec![String::new()];
     }
 
     // find index of every mine
-    let mine_locs = minefield
+    let mine_locs: Vec<(usize, usize)> = minefield
         .iter()
-        .map(|line| line.chars())
-        .flatten()
         .enumerate()
-        .filter_map(|(i, square)| if square == '*' { Some(i) } else { None });
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(move |(j, square)| match square {
+                    '*' => Some((i, j)),
+                    _ => None,
+                })
+        })
+        .flatten()
+        .collect();
 
     // for each mine, add 1 to surrounding tiles
     let mut board = BoardState::new(n_rows, n_cols);
