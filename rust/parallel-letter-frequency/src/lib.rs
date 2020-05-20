583c4e4925e92e9afb70e2d50d87576a45cce1b5
Colton Crowe <coltontcrowe@gmail.com>
2020-05-19 19:00:04 -0700

    Updated parallel-letter-frequency exercise on rust track to use rayon

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,41 +1,30 @@
+use rayon::prelude::*;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
-use crossbeam_utils::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+    let chunk_sz = input.len() / worker_count + 1;
+
+    let handles = input
+        .par_chunks(chunk_sz)
+        .map(|data| {
+            let mut part_count = HashMap::new();
+            for line in data {
+                update_from_str(&mut part_count, line);
+            }
+            part_count
+        })
+        .collect::<Vec<_>>();
+
     let mut counter = HashMap::new();
-    let _ = thread::scope(|scope| {
-        let input = Arc::new(Mutex::new(input.iter()));
-        let mut handles = Vec::new();
-
-        for _ in 0..worker_count {
-            let data = input.clone();
-            let mut part_count = HashMap::<char, usize>::new();
-            let handle = scope.spawn(move |_| {
-                loop {
-                    let line = data.lock().unwrap().next();
-                    match line {
-                        None => break,
-                        Some(content) => update_from_str(&mut part_count, content),
-                    }
-                }
-                part_count
-            });
-            handles.push(handle);
-        }
-
-        for handle in handles {
-            let received = handle.join().unwrap();
-            update_from_hash(&mut counter, received);
-        }
-    });
+    for received in handles {
+        update_from_hash(&mut counter, &received);
+    }
     counter
 }
 
-fn update_from_hash(main: &mut HashMap<char, usize>, other: HashMap<char, usize>) {
+fn update_from_hash(main: &mut HashMap<char, usize>, other: &HashMap<char, usize>) {
     other
-        .iter()
+        .into_iter()
         .for_each(|(&letter, count)| *main.entry(letter).or_insert(0) += count);
 }
 
