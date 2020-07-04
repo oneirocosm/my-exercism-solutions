9010d3e7476a95a38f16ba20a1af187bde8a03f8
Colton Crowe <coltontcrowe@gmail.com>
2020-07-03 17:06:32 -0700

    Remove unnecessary code in pangram exercise on javascript track

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,9 +3,5 @@
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
 
 export const isPangram = (text = "") => {
-  if (text == "") {
-    return false;
-  }
-
   return ALPHABET.every(letter => text.toLowerCase().includes(letter));
 };
