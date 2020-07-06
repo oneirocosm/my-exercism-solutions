35623efffc778786668c1e7ad611f9645979618b
Colton Crowe <coltontcrowe@gmail.com>
2020-07-05 22:20:31 -0700

    Update matrix exercise on the javascript track

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -3,22 +3,18 @@
 export class Matrix {
   constructor(data_text) {
     const rows_text = data_text.split("\n");
-    let rows = [];
-    for (const row_text of rows_text) {
-      rows.push(Matrix.parse_into_row(row_text));
-    }
+    const rows = rows_text.map(row_text => row_text.split(" ").map(Number));
     this._rows = rows;
 
-    const num_rows = rows.length;
     this._num_rows = rows.length;
-    this._num_cols = this.calc_col_length();
+    this._num_cols = this.calc_num_cols();
   }
 
   static parse_into_row(row_text) {
     return row_text.split(" ").map(Number);
   }
 
-  calc_col_length() {
+  calc_num_cols() {
     let num_cols;
     if (this._num_rows == 0) {
       num_cols = 0;
@@ -36,15 +32,8 @@ export class Matrix {
   }
 
   get columns() {
-    let cols = [];
-    for (let i = 0; i < this._num_cols; i++) {
-      let col = [];
-      for (let j = 0; j < this._num_rows; j++) {
-        col.push(this._rows[j][i]);
-      }
-      cols.push(col);
-    }
-
-    return cols;
+    return [...Array(this._num_cols).keys()].map(i =>
+      [...Array(this._num_rows).keys()].map(j => this._rows[j][i])
+    );
   }
 }
