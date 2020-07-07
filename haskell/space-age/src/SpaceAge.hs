d70fa52cf44b0deae966c7b093e7bb4828470504
Colton Crowe <coltontcrowe@gmail.com>
2020-07-06 21:50:19 -0700

    Initial solution to space age exercise on haskell track

---
+++ haskell/space-age/src/SpaceAge.hs
@@ -0,0 +1,30 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet = Mercury
+            | Venus
+            | Earth
+            | Mars
+            | Jupiter
+            | Saturn
+            | Uranus
+            | Neptune
+
+ageOn :: Planet -> Float -> Float
+ageOn planet seconds = 
+    case planet of
+        Earth -> seconds / 31557600.0
+        _     -> (ageOn Earth seconds) / (earthYrs planet)
+
+earthYrs :: Planet -> Float
+earthYrs planet =
+    case planet of
+        Mercury -> 0.2408467
+        Venus   -> 0.61519726
+        Earth   -> 1.0
+        Mars    -> 1.8808158
+        Jupiter -> 11.862615
+        Saturn  -> 29.447498
+        Uranus  -> 84.016846
+        Neptune -> 164.79132
+
+        
