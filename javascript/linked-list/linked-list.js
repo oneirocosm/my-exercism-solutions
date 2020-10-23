98cb56295642d4541f540d45a808b3843dc35b79
Colton Crowe <coltontcrowe@gmail.com>
2020-10-23 01:39:50 -0700

    Update linked list in the javascript chast with mentor suggestions

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,17 +1,16 @@
 export class LinkedList {
   constructor() {
-    this.head = undefined;
-    this.tail = undefined;
+    this.head = null;
+    this.tail = null;
   }
 
   push(elem) {
-    if (this.tail == undefined) {
-      const new_node = new Node(elem);
-      this.head = new_node;
-      this.tail = new_node;
+    const new_tail = new Node(elem);
+    if (this.tail === null) {
+      this.head = new_tail;
+      this.tail = new_tail;
     } else {
       const old_tail = this.tail;
-      const new_tail = new Node(elem);
       old_tail.next = new_tail;
       new_tail.prev = old_tail;
       this.tail = new_tail;
@@ -22,8 +21,8 @@ export class LinkedList {
     const old_tail = this.tail;
     const new_tail = old_tail.prev;
     this.tail = new_tail;
-    if (new_tail == undefined) {
-      this.head = undefined;
+    if (new_tail === null) {
+      this.head = null;
     }
     return old_tail.elem;
   }
@@ -32,20 +31,19 @@ export class LinkedList {
     const old_head = this.head;
     const new_head = old_head.next;
     this.head = new_head;
-    if (new_head == undefined) {
-      this.tail = undefined;
+    if (new_head === null) {
+      this.tail = null;
     }
     return old_head.elem;
   }
 
   unshift(elem) {
-    if (this.head == undefined) {
-      const new_node = new Node(elem);
-      this.head = new_node;
-      this.tail = new_node;
+    const new_head = new Node(elem);
+    if (this.head === null) {
+      this.head = new_head;
+      this.tail = new_head;
     } else {
       const old_head = this.head;
-      const new_head = new Node(elem);
       old_head.prev = new_head;
       new_head.next = old_head;
       this.head = new_head;
@@ -54,14 +52,14 @@ export class LinkedList {
 
   delete(elem) {
     let node = this.head;
-    while (node != undefined) {
+    while (node !== null) {
       if (node.elem == elem) {
         const prev = node.prev;
         const next = node.next;
-        if (next != undefined) {
+        if (next !== null) {
           next.prev = prev;
         }
-        if (prev != undefined) {
+        if (prev !== null) {
           prev.next = next;
         }
         break;
@@ -79,7 +77,7 @@ export class LinkedList {
   count() {
     let count = 0;
     let node = this.head;
-    while (node != undefined) {
+    while (node !== null) {
       node = node.next;
       ++count;
     }
@@ -90,7 +88,7 @@ export class LinkedList {
 class Node {
   constructor(elem) {
     this.elem = elem;
-    this.next = undefined;
-    this.prev = undefined;
+    this.next = null;
+    this.prev = null;
   }
 }
