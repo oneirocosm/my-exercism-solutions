bbcc1a96bb2503cec192e72de55466ff7cc9df6a
Colton Crowe <coltontcrowe@gmail.com>
2020-12-08 21:41:35 -0800

    Complete grade-school exercise on javascript track

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,22 @@
+export class GradeSchool {
+  constructor() {
+    this.db = {};
+  }
+  roster() {
+    return JSON.parse(JSON.stringify(this.db));
+  }
+
+  add(student, grade) {
+    if (this.db[grade]) {
+      this.db[grade].push(student);
+      this.db[grade].sort();
+    } else {
+      this.db[grade] = [student];
+    }
+  }
+
+  grade(num) {
+    const students = this.db[num] || [];
+    return students.map(x => x);
+  }
+}
