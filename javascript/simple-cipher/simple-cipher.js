44efd36e2248548c6e035d10c812feca12fe4abd
coltontcrowe <coltontcrowe@gmail.com>
2021-03-19 02:36:30 -0700

    Update simple cipher on js track with more mentor feedback

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,61 +1,71 @@
 export class Cipher {
   constructor(key = "") {
     if (!key) {
-      const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-      for (let i = 0; i < 10; i++) {
-        key += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
-      }
+      key = Cipher.randomKey();
     }
     this.key = key.toLowerCase();
   }
 
   encode(message) {
-    const offsets = keyValue(this.key);
-    return message.split("").reduce((acc, character) => {
-      const shift = offsets.next().value;
-      const newCharacter = translateChar(character, shift);
-      return acc + newCharacter;
-    }, "");
+    const offsets = this.keyOffsetGenerator();
+    return message
+      .split("")
+      .map(character => {
+        const shift = offsets.next().value;
+        return Cipher.translateChar(character, shift);
+      })
+      .join("");
   }
 
   decode(message) {
-    const offsets = keyValue(this.key);
-    return message.split("").reduce((acc, character) => {
-      const shift = 26 - offsets.next().value;
-      const newCharacter = translateChar(character, shift);
-      return acc + newCharacter;
-    }, "");
-  }
-}
-
-function* keyValue(key) {
-  const chars = key.split("");
-  const length = chars.length;
-  const ordBase = "a".charCodeAt(0);
-  let i = 0;
-  while (true) {
-    const ord = chars[i].charCodeAt(0) - ordBase;
-    yield ord;
-    i = (i + 1) % length;
-  }
-}
-
-function translateChar(character, shift) {
-  if (!isLetter) {
-    return character;
+    const offsets = this.keyOffsetGenerator();
+    return message
+      .split("")
+      .map(character => {
+        const shift = 26 - offsets.next().value;
+        return Cipher.translateChar(character, shift);
+      })
+      .join("");
   }
 
-  const ordLetter = isLowerCase(character) ? "a" : "A";
-  const ordBase = ordLetter.charCodeAt(0);
-  const ord = character.charCodeAt(0) - ordBase;
-  const ordShift = (ord + shift) % 26;
-  return String.fromCharCode(ordBase + ordShift);
-}
+  *keyOffsetGenerator() {
+    const length = this.key.length;
+    const ordBase = "a".charCodeAt(0);
+    let i = 0;
+    while (true) {
+      const ord = this.key[i].charCodeAt(0) - ordBase;
+      yield ord;
+      i = (i + 1) % length;
+    }
+  }
 
-function isLetter(string) {
-  return string.toLowerCase() != string.toUpperCase();
-}
+  static randomKey() {
+    const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    let key = "";
 
-function isLowerCase(string) {
-  return isLetter(string) && string == string.toLowerCase();
+    for (let i = 0; i < 10; i++) {
+      key += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return key;
+  }
+
+  static translateChar(character, shift) {
+    if (!Cipher.isLetter(character)) {
+      return character;
+    }
+
+    const baseLetter = Cipher.isLowerCase(character) ? "a" : "A";
+    const baseOrd = baseLetter.charCodeAt(0);
+    const relativeInputOrd = character.charCodeAt(0) - baseOrd;
+    const relativeShiftOrd = (relativeInputOrd + shift) % 26;
+    return String.fromCharCode(baseOrd + relativeShiftOrd);
+  }
+
+  static isLetter(string) {
+    return string.toLowerCase() != string.toUpperCase();
+  }
+
+  static isLowerCase(string) {
+    return Cipher.isLetter(string) && string == string.toLowerCase();
+  }
 }
