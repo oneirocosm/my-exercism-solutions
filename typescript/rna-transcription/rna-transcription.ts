dbf68b80f51c2f6ba5ae5a09c97082b77ef732d0
coltontcrowe <coltontcrowe@gmail.com>
2021-05-14 17:52:12 -0700

    Apply mentor feedback to rna-transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,8 @@
+const RNA_LETTERS = ["C", "G", "A", "U"];
+const DNA_LETTERS = ["C", "G", "A", "U"];
+export type RnaValid = typeof RNA_LETTERS[number];
+export type DnaValid = typeof DNA_LETTERS[number];
+
 class Transcriptor {
   static DNA_TO_RNA: { [key: string]: string } = {
     G: "C",
@@ -6,16 +11,14 @@ class Transcriptor {
     A: "U"
   };
 
-  toRna(dnaStrand: string): string {
-    return Array.from(dnaStrand)
-      .map(nucleotide => {
-        const rna = Transcriptor.DNA_TO_RNA[nucleotide];
-        if (rna === undefined) {
-          throw "Invalid input DNA.";
-        }
-        return rna;
-      })
-      .join("");
+  toRna(dnaStrand: DnaValid): RnaValid {
+    return Array.from(dnaStrand).reduce((acc_strand, nucleotide) => {
+      const rna: RnaValid | undefined = Transcriptor.DNA_TO_RNA[nucleotide];
+      if (rna === undefined) {
+        throw "Invalid input DNA.";
+      }
+      return acc_strand + rna;
+    }, "");
   }
 }
 
